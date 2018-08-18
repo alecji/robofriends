688b935943601a594c08f6de15d5e04d045e91d2
Alec Inthalangsy <alecinthalangsy@Alecs-MacBook-Air.local>
2018-08-18 14:44:11 -0400

    dynmmically display cards

--- src/Card.js
+++ src/Card.js
@@ -3,13 +3,13 @@ import React from 'react';
 const Card = ({id, name, email}) => {
     return (
         <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5' id={id}>
-            <img alt='robots ' src='https://robohash.org/test?200x200' />
+            <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
             </div>
         </div>
-    )
+    );
 };
 
 export default Card;
